44fb4d6a0439213b779c88d9baea2918110a966e
theoesberg <esbergtheo@gmail.com>
2025-05-07 18:09:47 +0200

    Added a simple character
    Added a simple player character that you can movearound and look around with

---
+++ VelocityShift/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensativity = 300f;
+    public Transform playerBody;
+
+    private float xRotation = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensativity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ VelocityShift/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement 
+    public CharacterController controller;
+    public float movementSpeed = 24f;
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+
+    // Groundcheck 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    // Jumping 
+    public float jumpHeight = 3f;
+
+    void Update()
+    {
+        Movement();
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * movementSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
